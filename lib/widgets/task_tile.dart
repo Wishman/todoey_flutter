8efa310e36ad2aa35322d27c15c51d155a808a87
Wishman <gjwisher@aol.com>
2020-01-04 17:06:25 +0100

    4. L-186: What is State and How do we Manage it? - turn TaskTile into stateless widget and move state management to parent (_TaskTileState) - create callbacks from TaskTile to _TasktileState to pass State and trigger re-build

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
 // 2.2 & 2.5 & 2.7
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false; // 4.4
+
+  /* obsolete with 4.10(d)
+  // 4.9
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+   */
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(value: false),
+      title: Text(
+        'This is a task',
+        style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null), // 4.8
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked, // 4.7
+        //toggleCheckboxState: checkboxCallback,  // 4.10 (c), obsolete with 4.10(d)
+        // 4.10(d)
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
+    );
+  }
+}
+
+//4.2 & 4.5
+class TaskCheckbox extends StatelessWidget {
+  // 4.6 & 4.10(a) only constr
+  final bool checkboxState;
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+
+  // 4.10(a)
+  final Function toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState, // <-- this is the STATE! of the Checkbox() & 4.3
+      onChanged: toggleCheckboxState, // 4.10(b)
     );
   }
 }
